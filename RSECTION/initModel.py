65e7c6f9b59956863b19ce93304f02585efcf612
heetpatel96 <heetpatel96@gmail.com>
2022-09-07 11:55:38 +0200

    update initModel.py

--- RSECTION/initModel.py
+++ RSECTION/initModel.py
@@ -37,3 +37,74 @@ session = requests.Session()
 adapter = requests.adapters.HTTPAdapter(pool_connections=1, pool_maxsize=1)
 session.mount('http://', adapter)
 trans = RequestsTransport(session)
+
+class Model():
+    clientModel = None
+    clientModelLst = []
+    activeSession = False
+
+    def __init__(self,
+                 new_model: bool=True,
+                 model_name: str="TestModel",
+                 delete: bool=False,
+                 delete_all: bool=False):
+        """
+        Class object representing individual model in RFEM.
+        Class enables to edit multiple models in one session through holding
+        one transport session active by not setting 'trans' into Client.
+        Args:
+            new_model (bool, optional): Set to True if new model is requested.
+            model_name (str, optional): Defaults to "TestModel".
+            delete (bool, optional):  Delete results
+            delete_all (bool, optional): Delete all objects in Model.
+        """
+
+        cModel = None
+        modelLs = client.service.get_model_list()
+
+        if new_model:
+            if modelLs and model_name in modelLs.name:
+                modelIndex = 0
+                for i,j in enumerate(modelLs.name):
+                    if modelLs.name[i] == model_name:
+                        modelIndex = i
+                new = client.service.get_model(modelIndex) + 'wsdl'
+                # Set transport parameter if it is the first model
+                if Model.activeSession:
+                    cModel = Client(new)
+                else:
+                    cModel = Client(new, transport=trans)
+                cModel.service.delete_all_results()
+                cModel.service.delete_all()
+            else:
+                new = client.service.new_model(model_name) + 'wsdl'
+                if Model.activeSession:
+                    cModel = Client(new)
+                else:
+                    cModel = Client(new, transport=trans)
+                if not modelLs:
+                    Model.activeSession = True
+        else:
+            modelIndex = 0
+            for i,j in enumerate(modelLs.name):
+                if modelLs.name[i] == model_name:
+                    modelIndex = i
+            new = client.service.get_model(modelIndex) + 'wsdl'
+            if Model.activeSession:
+                cModel = Client(new)
+            else:
+                cModel = Client(new, transport=trans)
+            if delete:
+                print('Deleting results...')
+                cModel.service.delete_all_results()
+            if delete_all:
+                print('Delete all...')
+                cModel.service.delete_all()
+
+        # when using multiple intances/model
+        self.clientModel = cModel
+        if not modelLs or not model_name in modelLs.name:
+            Model.clientModelLst.append(cModel)
+        # when using only one instace/model
+        Model.clientModel = cModel
+
