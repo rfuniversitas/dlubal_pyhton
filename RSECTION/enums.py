30d992d7409caa19ceba92712738b09008e21ded
heetpatel96 <heetpatel96@gmail.com>
2022-09-08 10:07:08 +0200

    added material, section

--- RSECTION/enums.py
+++ RSECTION/enums.py
@@ -1,1 +1,9 @@
+from enum import Enum
 
+
+class ObjectTypes(Enum):
+    '''
+    Object Types
+    '''
+
+    E_OBJECT_TYPE_MATERIAL, E_OBJECT_TYPE_SECTION = range(2)
--- example_RSECTION/demo.py
+++ example_RSECTION/demo.py
@@ -10,6 +10,8 @@ sys.path.append(dirName + r'/..')
 
 
 from RSECTION.initModel import Model
+from RSECTION.BasicObjects.material import Material
+from RSECTION.BasicObjects.section import Section
 from RSECTION.BasicObjects.point import Point
 
 
@@ -18,5 +20,8 @@ if __name__ == '__main__':
     Model(True, "Demo") # crete new model called Demo1
     Model.clientModel.service.begin_modification()
 
-    # Point(1, 0.0, 0.02)
-    # Point(2, 0.2, 0,0)
+    Point(1, 0.0, 0.02)
+    Point(2, 0.2, 0,0)
+
+    Material(1, 'S235')
+    Section(1, 'IPE 200')
