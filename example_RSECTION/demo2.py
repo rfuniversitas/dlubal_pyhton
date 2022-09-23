4f8acc4aef8d7745682fe43795ab48a70aa8cfa0
heetpatel96 <heetpatel96@gmail.com>
2022-09-23 10:56:06 +0200

    update

--- example_RSECTION/demo2.py
+++ example_RSECTION/demo2.py
@@ -9,7 +9,7 @@ print('dirname:     ', dirName)
 sys.path.append(dirName + r'/..')
 
 
-from RSECTION.initModel import Model
+from RSECTION.initModel import Calculate_all, Model
 from RSECTION.BasicObjects.material import Material
 from RSECTION.BasicObjects.section import Section
 from RSECTION.BasicObjects.point import Point
@@ -62,9 +62,11 @@ if __name__ == '__main__':
 
     StressPoint(1, -0.3, 0.75)
     StressPoint.Standard(2, [0.6, 0], 1)
-    StressPoint.OnElement(3, 1, ElementSide.ELEMENT_SIDE_MIDDLE, PointReferenceType.REFERENCE_TYPE_L, [True, 0.4])
-    StressPoint.OnLine(4, 1, PointReferenceType.REFERENCE_TYPE_L, [True, 0.4])
+    # StressPoint.OnElement(3, 1, ElementSide.ELEMENT_SIDE_MIDDLE, PointReferenceType.REFERENCE_TYPE_L, [True, 0.4])
+    # StressPoint.OnLine(4, 1, PointReferenceType.REFERENCE_TYPE_L, [True, 0.4])
 
 
     Model.clientModel.service.finish_modification()
+
+    Calculate_all()
 
